679a7efc9e84a63d625505f20c052fd178f458eb
Tarik-Talic <talic.tarik@gmail.com>
2023-04-02 19:07:47 +0200

    Badge Fix

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,9 +2,15 @@ import React from "react";
 import "./Card.css";
 
 export default function Card(props) {
+  let badgeText;
+  if (props.openSpots === 0) {
+    badgeText = "SOLD OUT";
+  } else if (props.location === "Online") {
+    badgeText = "ONLINE";
+  }
   return (
     <div className="card">
-      {props.openSpots === 0 && <div className="card--badge">SOLD OUT</div>}
+      {badgeText && <div className="card--badge">{badgeText}</div>}
       <img className="card--image" src={props.img} alt="Profile" />
       <div className="cards--status">
         <img
