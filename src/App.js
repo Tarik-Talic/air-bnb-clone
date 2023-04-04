ea292efc8f112f4e055c4797c732e5c6e0d6b7bd
Tarik-Talic <talic.tarik@gmail.com>
2023-04-04 02:31:25 +0200

    Props Fixing

--- src/App.js
+++ src/App.js
@@ -6,18 +6,7 @@ import Card from "./components/Card";
 
 export default function App() {
   const cardElements = cardData.map((data) => {
-    return (
-      <Card
-        key={data.id}
-        img={data.coverImg}
-        rating={data.stats.rating}
-        reviewCount={data.stats.reviewCount}
-        location={data.location}
-        title={data.title}
-        price={data.price}
-        openSpots={data.openSpots}
-      />
-    );
+    return <Card key={data.id} item={data} />;
   });
   return (
     <>
--- src/components/Card.js
+++ src/components/Card.js
@@ -3,28 +3,28 @@ import "./Card.css";
 
 export default function Card(props) {
   let badgeText;
-  if (props.openSpots === 0) {
+  if (props.item.openSpots === 0) {
     badgeText = "SOLD OUT";
-  } else if (props.location === "Online") {
+  } else if (props.item.location === "Online") {
     badgeText = "ONLINE";
   }
   return (
     <div className="card">
       {badgeText && <div className="card--badge">{badgeText}</div>}
-      <img className="card--image" src={props.img} alt="Profile" />
+      <img className="card--image" src={props.item.coverImg} alt="Profile" />
       <div className="cards--status">
         <img
           className="card--star"
           src={process.env.PUBLIC_URL + "/images/Star 1.png"}
           alt="star"
         />
-        <span>{props.rating}</span>
-        <span className="grey">({props.reviewCount}) &bull;</span>
-        <span className="grey">{props.location}</span>
+        <span>{props.item.stats.rating}</span>
+        <span className="grey">({props.item.stats.reviewCount}) &bull;</span>
+        <span className="grey">{props.item.location}</span>
       </div>
-      <p> {props.title}</p>
+      <p> {props.item.title}</p>
       <p>
-        <b>From ${props.price}</b> / person
+        <b>From ${props.item.price}</b> / person
       </p>
     </div>
   );
